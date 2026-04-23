73b2b0df44e99b75250e4a46dfaa6c991c70bcdc
JuanBergonzi <juan.bergonzi.ices@gmail.com>
2026-04-23 18:17:37 -0300

    commit

--- AnalisisNumerico2026/Calculadora/obj/Debug/net8.0/Logica.AssemblyInfo.cs
+++ AnalisisNumerico2026/Calculadora/obj/Debug/net8.0/Logica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87293686bf2ca788e433864f99d3bec754d6fd95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076687c0651b54983cd7da2b033cffc312277251")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
